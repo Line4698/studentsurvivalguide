a53a5a1d89c70d1272162f1919c2a51c5bd8169f
Line4698 <eaalarjo@students.eaaa.dk>
2021-11-10 10:14:13 +0100

    done john

--- ssg.php
+++ ssg.php
@@ -11,46 +11,46 @@
 
  function plugintest () {
      $content = '<div id="pins">
-            <a href="https://www.studentsurvivalguide.dk/posts/aarhus-survival-guide"><button type="button" id="aarhus" class="bounce-in-top">
-                    <span id="pin0tekst"></span>
-                </button>
-            </a>
-            <a href="https://www.studentsurvivalguide.dk/posts/survival-guide-to-aalborg"><button type="button" id="aalborg" class="bounce-in-top">
-                    <span id="pin1tekst"></span>
-                </button>
-            </a>
-            <a href="index.html"><button type="button" id="kobenhavn" class="bounce-in-top">
-                    <span id="pin2tekst"></span>
-                </button>
-            </a>
-            <a href="index.html"><button type="button" id="odense" class="bounce-in-top">
-                    <span id="pin3tekst"></span>
-                </button>
-            </a>
-            <a href="index.html"><button type="button" id="vejle" class="bounce-in-top">
-                    <span id="pin4tekst"></span>
-                </button>
-            </a>
-            <a href="https://www.studentsurvivalguide.dk/posts/roskilde-survival-guide"><button type="button" id="roskilde" class="bounce-in-top">
-                    <span id="pin5tekst"></span>
-                </button>
-            </a>
-            <a href="index.html"><button type="button" id="esbjerg" class="bounce-in-top">
-                    <span id="pin6tekst"></span>
-                </button>
-            </a>
-            <a href="index.html"><button type="button" id="kolding" class="bounce-in-top">
-                    <span id="pin7tekst"></span>
-                </button>
-            </a>
-            <a href="index.html"><button type="button" id="sonderborg" class="bounce-in-top">
-                    <span id="pin8tekst"></span>
-                </button>
-            </a>
-            <a href="index.html"><button type="button" id="randers" class="bounce-in-top">
-                    <span id="pin9tekst"></span>
-                </button>
-            </a>
+            <button type="button" id="aarhus" class="bounce-in-top">
+            <span id="pin0tekst"></span>
+            </button>
+            
+            <button type="button" id="aalborg" class="bounce-in-top">
+            <span id="pin1tekst"></span>
+            </button>
+            
+            <button type="button" id="kobenhavn" class="bounce-in-top">
+                <span id="pin2tekst"></span>
+            </button>
+            
+            <button type="button" id="odense" class="bounce-in-top">
+                <span id="pin3tekst"></span>
+            </button>
+            
+            <button type="button" id="vejle" class="bounce-in-top">
+                <span id="pin4tekst"></span>
+            </button>
+            
+            <button type="button" id="roskilde" class="bounce-in-top">
+                <span id="pin5tekst"></span>
+            </button>
+            
+            <button type="button" id="esbjerg" class="bounce-in-top">
+                <span id="pin6tekst"></span>
+            </button>
+            
+            <button type="button" id="kolding" class="bounce-in-top">
+                <span id="pin7tekst"></span>
+            </button>
+            
+            <button type="button" id="sonderborg" class="bounce-in-top">
+                <span id="pin8tekst"></span>
+            </button>
+            
+            <button type="button" id="randers" class="bounce-in-top">
+                <span id="pin9tekst"></span>
+            </button>
+            
         </div>';
      return $content;
  }
